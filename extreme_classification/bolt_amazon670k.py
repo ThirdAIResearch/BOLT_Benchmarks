06b237a5e7e82496cfb12ecec27d21cc937a35cb
Josh Engels <josh@thirdai.com>
2023-03-20 17:27:38 -0500

    Refactoring out download

--- extreme_classification/bolt_amazon670k.py
+++ extreme_classification/bolt_amazon670k.py
@@ -1,18 +1,14 @@
-import gdown
-import shutil
-import os
 
-def download_dataset():
-    train_file_loc = "Amazon670k/train.txt"
-    test_file_loc = "Amazon670k/test.txt"
-    if not os.path.exists(train_file_loc) or not os.path.exists(test_file_loc):
-        gdown.download(
-            "https://drive.google.com/u/0/uc?id=1TLaXCNB_IDtLhk4ycOnyud0PswWAW6hR&export=download",
-            output="Amazon670k.zip",
-        )
-        shutil.unpack_archive("Amazon670k.zip", "Amazon670k")
-    return train_file_loc, test_file_loc
+import thirdai
+from extreme_download import download_amazon_670k
 
+
+def train_model(train_file, test_file, num_epochs, learning_rate):
+    model = thirdai.bolt.UniversalDeepTransformer(file_format="svm", n_target_classes=670091, input_dim=135909)
+    model.train(train_file, epochs=num_epochs, learning_rate=learning_rate)
+    metrics = model.evaluate(test_file, metrics=["categorical_accuracy"])
+    print(metrics)
 
 if __name__ == "__main__":
-    train_file, test_file = download_dataset()
+    train_file, test_file = download_amazon_670k()
+    train_model(train_file=train_file, test_file=test_file, num_epochs=3, learning_rate=0.0001)
